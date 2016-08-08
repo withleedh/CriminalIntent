29f762fee2a53c7c7c4d8dae0014e52956af54e9
Lee Dong ho <withleedh@gmail.com>
2016-08-09 08:36:18 +0900

    add Button,checkbox to UI page

--- app/src/main/java/com/ge/appl/criminalintent/Crime.java
+++ app/src/main/java/com/ge/appl/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.ge.appl.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,13 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSoloved;
+
 
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -25,5 +30,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSoloved() {
+        return mSoloved;
+    }
+
+    public void setmSoloved(boolean mSoloved) {
+        this.mSoloved = mSoloved;
     }
 }
